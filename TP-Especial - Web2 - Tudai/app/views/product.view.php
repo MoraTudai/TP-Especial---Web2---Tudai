0922ea34d54b80e400fcc11b24784fa53af24096
MoraTudai <108840360+MoraTudai@users.noreply.github.com>
2022-11-16 00:13:03 -0300

    Update product.view.php

--- TP-Especial - Web2 - Tudai/app/views/product.view.php
+++ TP-Especial - Web2 - Tudai/app/views/product.view.php
@@ -8,39 +8,59 @@ class ProductView {
         $this->smarty = new Smarty(); // inicializo Smarty
     }
 
-    function showProducts($productos) {
+    function showProducts($productos, $categorias) {
         // asigno variables al tpl smarty
        // $this->smarty->assign('count', count($productos)); 
        $this->smarty->assign('count', count($productos));
        $this->smarty->assign('productos', $productos);
+       $this->smarty->assign('categorias', $categorias);
 
         // mostrar el tpl
         $this->smarty->display('productList.tpl');
     }
 
-    function showEdit($id_product) {
-        
+    function showProductDetail($detalleProducto) {
         // asigno variables al tpl smarty
-        // $this->smarty->assign('count', count($productos)); 
-        $this->smarty->assign('id_product', $id_product);
+       $this->smarty->assign('count', count($detalleProducto));
+       $this->smarty->assign('detalleProducto', $detalleProducto); 
 
         // mostrar el tpl
+        $this->smarty->display('productDetail.tpl');
+    }
+
+    function showEdit($productoAModificar, $categorias) {
+        
+        // asigno variables al tpl smarty
+        $this->smarty->assign('productoAModificar', $productoAModificar);
+        $this->smarty->assign('categorias', $categorias);
+    //    $this->smarty->assign('name_product', $name_product);
+      //  $this->smarty->assign('size', $size);
+        //$this->smarty->assign('color', $color);
+        //$this->smarty->assign('price', $price);
+        //$this->smarty->assign('id_categorie_fk', $id_categorie_fk);
+
+        // mostrar el tpl
+        $this->smarty->display('product_to_modify.tpl');
         $this->smarty->display('form_edit_product.tpl');
     }
 
-    function printEdit($editarproductos, $productos) {
+    function printEdit($editarproductos, $productos, $categorias) {
         // asigno variables al tpl smarty
         $this->smarty->assign('count', count($productos));
         $this->smarty->assign('editarproductos', $editarproductos);
         $this->smarty->assign('productos', $productos);
+        $this->smarty->assign('categorias', $categorias);
 
         // mostrar el tpl
         $this->smarty->display('productList.tpl');
     }
 
     function showError($message){
-        echo "<h1>ERROR!</h1>";
-        echo "<h2>$message</h2>";
+        // asigno variables al tpl smarty
+        $this->smarty->assign('message', $message);
+        
+        // mostrar el tpl
+        $this->smarty->display('error.tpl');
     }
 
    
