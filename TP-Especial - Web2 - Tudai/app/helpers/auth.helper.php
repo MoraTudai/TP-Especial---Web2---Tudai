09004c59b6a8601a53fac93206cbb438887933dc
MoraTudai <108840360+MoraTudai@users.noreply.github.com>
2022-11-16 00:11:09 -0300

    Update auth.helper.php

--- TP-Especial - Web2 - Tudai/app/helpers/auth.helper.php
+++ TP-Especial - Web2 - Tudai/app/helpers/auth.helper.php
@@ -1,16 +1,17 @@
 <?php
 
 class AuthHelper {
-                                                                                                                                                                                    
-     /**
-     * Verifica que el user este logueado y si no lo está
-     * lo redirige al login.
-     */
-    public function checkLoggedIn() {
+
+    public function __construct(){
         session_start();
+    }
+                                                                                                                                                                   
+     /*Verifica que el user este logueado y si no lo está lo redirige al login*/
+    public function checkLoggedIn() {
+        
         if (!isset($_SESSION['IS_LOGGED'])) {
             header("Location: " . BASE_URL . 'login');
-           
+            die();
         }
     } 
     
