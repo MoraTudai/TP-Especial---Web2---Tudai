35b018846b18a9473f2b27779c2dec84b8e9eaec
MoraTudai <108840360+MoraTudai@users.noreply.github.com>
2022-11-16 00:12:23 -0300

    Update product.model.php

--- TP-Especial - Web2 - Tudai/app/models/product.model.php
+++ TP-Especial - Web2 - Tudai/app/models/product.model.php
@@ -8,14 +8,14 @@ class ProductModel {
         $this->db = new PDO('mysql:host=localhost;'.'dbname=db_store;charset=utf8', 'root', '');
     }
 
-    /*Devuelve la lista de tareas completa*/
+    /*Devuelve todos los productos de la tabla*/
     public function getAllProducts() {
         // 1. abro conexión a la DB
         // ya esta abierta por el constructor de la clase
 
         // 2. ejecuto la sentencia (2 subpasos)
-        $query = $this->db->prepare("SELECT * FROM products");
-        $query->execute();
+        $query = $this->db->prepare('SELECT products.*, categories.nombre as categoria FROM products JOIN categories ON products.id_categorie_fk = categories.id_categorie');
+        $query->execute();         //SELECT products.*, categories.nombre as categoria FROM products JOIN categories ON products.id_categorie_fk = categories.id_categorie
 
         // 3. obtengo los resultados
         $productos = $query->fetchAll(PDO::FETCH_OBJ); // devuelve un arreglo de objetos
@@ -23,22 +23,51 @@ class ProductModel {
         return $productos;
     }
 
+    /*Devuelve el detalle de un producto determinado por su ID*/
+    function getProductDetail($id) {
+        $query = $this->db->prepare('SELECT products.*, categories.nombre as categoria FROM products JOIN categories ON products.id_categorie_fk = categories.id_categorie WHERE id_product=?');
+        $query->execute([$id]);//SELECT products.*, categories.nombre as categoria FROM products JOIN categories ON products.id_categorie_fk = categories.id_categorie WHERE id_product=?;
+
+        // 3. obtengo los resultados
+        $detalleProducto = $query->fetchAll(PDO::FETCH_OBJ); // devuelve un arreglo de objetos
+        
+        return $detalleProducto;
+
+    }
+    
     /*Inserta un producto en la base de datos*/
-    function insertProduct($name_product, $size, $color, $price, $id_categorie_fk){
-        $query = $this->db->prepare("INSERT INTO products (name_product, size, color, price, id_categorie_fk) VALUES ( ?, ?, ?, ?, ?)");
-        $query->execute([$name_product, $size, $color, $price, $id_categorie_fk]);      
+    function insertProduct($name_product, $size, $color, $price, $id_categorie_fk, $description){
+        $query = $this->db->prepare("INSERT INTO products (name_product, size, color, price, id_categorie_fk, description) VALUES ( ?, ?, ?, ?, ?, ?)");
+        $query->execute([$name_product, $size, $color, $price, $id_categorie_fk, $description]);      
 
         return $this->db->lastInsertId();
     }
 
-    public function editProduct($id, $name_product, $size, $color, $price, $id_categorie_fk) {
-        $editarproductos = $this->db->prepare("UPDATE products SET name_product=?,size=?,color=?,price=?,id_categorie_fk=? WHERE id_product=?");
+    /*Devuelve el producto identificado por su ID*/
+    function productToModify($id) {
+        // 1. abro conexión a la DB
+        // ya esta abierta por el constructor de la clase
+
+        // 2. ejecuto la sentencia (2 subpasos)
+        $query = $this->db->prepare('SELECT products.*, categories.nombre as categoria FROM products JOIN categories ON products.id_categorie_fk = categories.id_categorie WHERE id_product=?');
+        $query->execute([$id]);//SELECT products.*, categories.nombre as categoria FROM products JOIN categories ON products.id_categorie_fk = categories.id_categorie WHERE id_product=?;
+
+        // 3. obtengo los resultados
+        $productoAModificar = $query->fetchAll(PDO::FETCH_OBJ); // devuelve un arreglo de objetos
+        
+        return $productoAModificar;
+
+    }
+
+    public function editProduct($name_product, $size, $color, $price, $id_categorie_fk, $description, $id) {
+        $editarproductos = $this->db->prepare("UPDATE products SET name_product = ?, size = ?, color = ?, price = ?, id_categorie_fk = ?, description = ? WHERE id_product=?");
         //('UPDATE products SET name_product=?, size=?, color=?, price=?, id_categorie_fk=? WHERE id_product = ?');
-        $editarproductos->execute([$id, $name_product, $size, $color, $price, $id_categorie_fk]); //nombre-de-la-columna = valor[, nombre-de-la-columna=valor]
+        $editarproductos->execute([$id, $name_product, $size, $color, $price, $id_categorie_fk, $description]); //nombre-de-la-columna = valor[, nombre-de-la-columna=valor]
         //var_dump($query->errorInfo()); // y eliminar la redireccion
         return $editarproductos;
     }
 
+    
     /*Elimina un producto dado su id*/
     function deleteProduct($id_product) {//consulta desde SQL -> DELETE FROM `products` WHERE `products`.`id_product` = 22;
         $query = $this->db->prepare('DELETE FROM products WHERE id_product = ?');
@@ -47,3 +76,4 @@ class ProductModel {
 
    
 }
+
