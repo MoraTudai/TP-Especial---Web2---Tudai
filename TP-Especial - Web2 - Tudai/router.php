153fb80e84dbbb9d7743a9ddd4672bf9d6b12627
MoraTudai <108840360+MoraTudai@users.noreply.github.com>
2022-11-16 00:03:24 -0300

    Update router.php

--- TP-Especial - Web2 - Tudai/router.php
+++ TP-Especial - Web2 - Tudai/router.php
@@ -1,12 +1,11 @@
 <?php
 require_once './app/controllers/product.controller.php';
 require_once './app/controllers/categorie.controller.php';
-require_once './app/controllers/productsbycategorie.controller.php';
 require_once './app/controllers/auth.controller.php';
 
 define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
 
-$action = 'login'; // acción por defecto
+$action = 'listProduct'; // acción por defecto
 if (!empty($_GET['action'])) {
     $action = $_GET['action'];
 }
@@ -14,53 +13,46 @@ if (!empty($_GET['action'])) {
 // parsea la accion Ej: dev/juan --> ['dev', juan]
 $params = explode('/', $action);
 
+$authController = new AuthController();
+$productController = new ProductController();
+$categorieController = new CategorieController();
+
 // tabla de ruteo
 switch ($params[0]) {
+    //login
     case 'login':
-        $authController = new AuthController();
         $authController->showFormLogin();
         break;
     case 'validate':
-        $authController = new AuthController();
         $authController->validateUser();
         break;
     case 'logout':
-        $authController = new AuthController();
         $authController->logout();
         break;
     case 'listProduct':
-        $productController = new ProductController();
         $productController->showProducts();
         break;
-    //case 'productDetail':
-    //    $productController = new ProductController();
-    //    $productController->showDetailProduct();
-    //    break;
     case 'listCategorie':
-        $categorieController = new CategorieController();
         $categorieController->showCategories();
         break;
-    case 'productsByCategorie':
-        $productsByCategorieController = new ProductsByCategorieController();
-        $productsByCategorieController->showProductsByCategorie();
-        break;
+    case 'showByProduct':
+        $id = $params[1];
+        $productController->showProduct($id);
+        break;   
     case 'insertProduct':
-        $productController = new ProductController();
         $productController->insertProduct();
         break;
     case 'insertCategorie':
-        $categorieController = new CategorieController();
         $categorieController->insertCategorie();
         break;
 
     //edición de producto - 2 pasos -
     case 'showEdit':
-        $productController = new ProductController();
         $id = $params[1];//$id_product = $_POST['id_product'];
         $productController->showEdit($id);
         break;
+        
     case 'editProduct': 
-        $productController = new ProductController();
         $id = $_POST['id'];//$id = $params[0];$id_product = $_POST['id_product'];
         $productController->editProduct($id);
         break;
@@ -68,25 +60,21 @@ switch ($params[0]) {
 
     //edición de categoria - 2 pasos -
     case 'showEditCategorie':
-        $categorieController = new CategorieController();
         $id = $params[1];//$id_product = $_POST['id_product'];
         $categorieController->showEditCategorie($id);
         break;
     case 'editCategorie': 
-        $categorieController = new CategorieController();
         $id = $_POST['id'];
         $categorieController->editCategorie($id);
         break;
     //fin edición de categoria - 
 
     case 'deleteProduct':
-        $productController = new ProductController();
         // obtengo el parametro de la acción
         $id = $params[1];
         $productController->deleteProduct($id);
         break;
     case 'deleteCategorie':
-        $categorieController = new CategorieController();
         // obtengo el parametro de la acción
         $id = $params[1];
         $categorieController->deleteCategorie($id);
@@ -95,3 +83,4 @@ switch ($params[0]) {
         echo('404 Page not found');
         break;
 }
+
